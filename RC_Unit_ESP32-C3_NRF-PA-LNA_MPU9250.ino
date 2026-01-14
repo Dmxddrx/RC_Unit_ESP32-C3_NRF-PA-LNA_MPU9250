db6a4d70c9ace6d1659e5589c581001aa9ad1d8b
Dmxddrx <dmxddrx5@gmail.com>
2026-01-14 17:39:49 +0530

    test

--- RC_Unit_ESP32-C3_NRF-PA-LNA_MPU9250.ino
+++ RC_Unit_ESP32-C3_NRF-PA-LNA_MPU9250.ino
@@ -1,54 +1,54 @@
-#include "MPU9250.h"
-#include <math.h>
+#include <Wire.h>
+#include <MPU9250_asukiaaa.h>
 
-MPU9250 mpu;
+#define SDA_PIN 6
+#define SCL_PIN 7
+
+MPU9250_asukiaaa mpu;
 
 void setup() {
-    Serial.begin(115200);
-    Wire.begin(8, 9); // SDA = GPIO 8, SCL = GPIO 9 for ESP32-C3
+  Serial.begin(115200);
+  while (!Serial);
 
-    delay(2000);
+  Wire.begin(SDA_PIN, SCL_PIN);
+  Wire.setClock(100000);
 
-    if (!mpu.setup(0x68)) {  // Change to 0x69 if your jumper is set high
-        while (1) {
-            Serial.println("MPU9250 connection failed. Check wiring!");
-            delay(5000);
-        }
-    }
-    
-    Serial.println("MPU9250 Initialized.");
+  Serial.println("Starting MPU9250...");
+
+  mpu.setWire(&Wire);
+  mpu.beginAccel();
+  mpu.beginGyro();
+  mpu.beginMag();
+
+  Serial.println("✅ MPU9250 initialized");
 }
 
 void loop() {
-    if (mpu.update()) {
-        // 1. Get Raw/Scaled Values
-        float ax = mpu.getAccX();
-        float ay = mpu.getAccY();
-        float az = mpu.getAccZ();
-        
-        float gx = mpu.getGyroX();
-        float gy = mpu.getGyroY();
-        float gz = mpu.getGyroZ();
-        
-        float mx = mpu.getMagX();
-        float my = mpu.getMagY();
-        float mz = mpu.getMagZ();
+  mpu.accelUpdate();
+  mpu.gyroUpdate();
+  mpu.magUpdate();
 
-        // 2. Calculate Pitch & Roll from Accelerometer
-        // Roll = atan2(Y, Z) | Pitch = atan2(-X, sqrt(Y^2 + Z^2))
-        float roll  = atan2(ay, az) * 180.0 / PI;
-        float pitch = atan2(-ax, sqrt(ay * ay + az * az)) * 180.0 / PI;
+  Serial.print("ACC: ");
+  Serial.print(mpu.accelX(), 2);
+  Serial.print(", ");
+  Serial.print(mpu.accelY(), 2);
+  Serial.print(", ");
+  Serial.print(mpu.accelZ(), 2);
 
-        // 3. Print Everything
-        Serial.print("--- TILT ---  ");
-        Serial.print("P: "); Serial.print(pitch, 1);
-        Serial.print("  R: "); Serial.println(roll, 1);
+  Serial.print(" | GYRO: ");
+  Serial.print(mpu.gyroX(), 2);
+  Serial.print(", ");
+  Serial.print(mpu.gyroY(), 2);
+  Serial.print(", ");
+  Serial.print(mpu.gyroZ(), 2);
 
-        Serial.print("ACCEL: "); Serial.print(ax); Serial.print(", "); Serial.print(ay); Serial.print(", "); Serial.println(az);
-        Serial.print("GYRO:  "); Serial.print(gx); Serial.print(", "); Serial.print(gy); Serial.print(", "); Serial.println(gz);
-        Serial.print("MAG:   "); Serial.print(mx); Serial.print(", "); Serial.print(my); Serial.print(", "); Serial.println(mz);
-        
-        Serial.println("-----------------------------------------");
-    }
-    delay(200); // Small delay to make it readable
+  Serial.print(" | MAG: ");
+  Serial.print(mpu.magX(), 2);
+  Serial.print(", ");
+  Serial.print(mpu.magY(), 2);
+  Serial.print(", ");
+  Serial.print(mpu.magZ(), 2);
+
+  Serial.println();
+  delay(100);
 }
